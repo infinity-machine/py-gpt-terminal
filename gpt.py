cc8ade1ebe270639facdbcc7b8b6174e6c2fb424
Connor Carciofini <ccarciofini@gmail.com>
2023-04-05 10:59:26 -0500

    init

---
+++ gpt.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+messages = [{
+        "role": "system",
+        "content": "Just do your best you helpful boy you!"
+}]
+
+def __main__():
+    while True:
+        user_input = input('USER: ')
+        if user_input.lower() == 'exit':
+            print('SAYONARA!')
+            sys.exit()
+        if user_input:
+            messages.append({
+                    "role": "user",
+                    "content": user_input
+                })
+            chat_completion = openai.ChatCompletion.create(
+                model='gpt-3.5-turbo', messages=messages
+            )
+        gpt_response = chat_completion.choices[0].message.content
+        print(f"GPT: {gpt_response}")
+        messages.append({
+            'role': 'assistant',
+            'content': gpt_response
+        })
+        
+__main__()
